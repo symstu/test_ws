2a956e862b5f8bd464949588adf28a498744e257
Maksym Stukalo <stukalo.maksym@gmail.com>
2022-07-15 02:35:30 +0200

    fixed front host

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     DEBUG: bool = True
     MODE: str = 'dev'
-    HOST: str = 'http://localhost:8000'
+    HOST: str = 'localhost:8000'
 
     PG_DSN: str = 'postgresql://postgres:postgres@localhost:5432/test_ws'
     PG_DSN_ROOT: str = 'postgresql://postgres:postgres@localhost:5432/postgres'
